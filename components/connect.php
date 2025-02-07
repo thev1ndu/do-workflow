7377820010d3054ea61ee62e2d05f327e7fd283d
ThevinduW <120010745+thev1ndu@users.noreply.github.com>
2025-02-07 22:56:02 +0530

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -2,7 +2,7 @@
 
 $db_name = 'mysql:host=localhost; dbname=astro';
 $user_name = '${{ secrets.DB_USERNAME }}';
-$user_password = '${{ secrets.DO_PASSWORD }}';
+$user_password = '${{ secrets.DB_PASSWORD }}';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
